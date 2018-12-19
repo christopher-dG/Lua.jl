29efad6a52baaa85f75076daee59ea0527599b42
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2018-12-19 06:50:14 +0000

    build based on 53614d4

--- dev/search_index.js
+++ dev/search_index.js
@@ -9,6 +9,22 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "#Lua.@lua_str-Tuple{Any}",
+    "page": "Home",
+    "title": "Lua.@lua_str",
+    "category": "macro",
+    "text": "@lua_str -> Any\n\nEvaluate some Lua code. Variables interpolated with $ are made into local Lua variables.\n\nExamples\n\njulia> lua\"print(1)\";\n1\n\njulia> x = 1; lua\"print($x)\";\n1\n\n\n\n\n\n"
+},
+
+{
+    "location": "#Lua.@luatype-Tuple{Symbol}",
+    "page": "Home",
+    "title": "Lua.@luatype",
+    "category": "macro",
+    "text": "@luatype T\n\nEnable converting variables of type T into Lua variables.\n\n\n\n\n\n"
+},
+
+{
     "location": "#Lua.jl-1",
     "page": "Home",
     "title": "Lua.jl",
